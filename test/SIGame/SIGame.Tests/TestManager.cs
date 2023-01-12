6c1e31e9a392c8ce210f6b87f28c8ed8bb08b87c
VladimirKhil <vladimir.khil@gmail.com>
2023-01-12 22:39:00 +0100

    Fix build

--- test/SIGame/SIGame.Tests/TestManager.cs
+++ test/SIGame/SIGame.Tests/TestManager.cs
@@ -1,86 +1,94 @@
 ﻿using SICore;
 using SIGame.ViewModel.PlatformSpecific;
-using System;
 using System.Windows.Input;
 
-namespace SIGame.Tests
+namespace SIGame.Tests;
+
+internal sealed class TestManager : PlatformManager
 {
-	internal sealed class TestManager : PlatformManager
+	public override ICommand Close { get; } = new CustomCommand((arg) => { });
+
+	public override void Activate() { }
+
+	public override bool Ask(string text) => throw new NotImplementedException();
+
+	public override string GetKeyName(int key) => key.ToString();
+
+	public override void PlaySound(string sound = null, double speed = 1, bool loop = false)
 	{
-		public override ICommand Close { get; } = new CustomCommand((arg) => { });
-
-		public override void Activate() { }
-
-		public override bool Ask(string text) => throw new NotImplementedException();
-
-		public override string GetKeyName(int key) => key.ToString();
-
-		public override void PlaySound(string sound = null, double speed = 1, bool loop = false)
-		{
-			
-		}
-
-		public override string SelectColor() => throw new NotImplementedException();
-
-		public override string SelectHumanAvatar() => throw new NotImplementedException();
-
-		public override string SelectLocalPackage(long? maxPackageSize) => System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SIGameTest.siq");
-
-		public override string SelectLogsFolder(string initialFolder) => throw new NotImplementedException();
-
-		public override string SelectSettingsForExport()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override string SelectSettingsForImport()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override string SelectMainBackground()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override string SelectStudiaBackground()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override string SelectLogo()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override string SelectSound()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override void SendErrorReport(Exception exc, bool isWarning = false)
-		{
-			throw new NotImplementedException();
-		}
-
-		public override void ShowHelp(bool asDialog)
-		{
-			throw new NotImplementedException();
-		}
-
-		public override void ShowMessage(string text, MessageType messageType, bool uiThread = false)
-		{
-			throw new NotImplementedException();
-		}
-
-		public override IAnimatableTimer GetAnimatableTimer()
-		{
-			return new AnimatableTimerMock();
-		}
-
-		public override void ExecuteOnUIThread(Action action)
-		{
-			action();
-		}
+		
 	}
-}
+
+	public override string SelectColor() => throw new NotImplementedException();
+
+	public override string SelectHumanAvatar() => throw new NotImplementedException();
+
+	public override string SelectLocalPackage(long? maxPackageSize) => System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SIGameTest.siq");
+
+	public override string SelectLogsFolder(string initialFolder) => throw new NotImplementedException();
+
+	public override string SelectSettingsForExport()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string SelectSettingsForImport()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string SelectMainBackground()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string SelectStudiaBackground()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string SelectLogo()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override string SelectSound()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void SendErrorReport(Exception exc, bool isWarning = false)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void ShowHelp(bool asDialog)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void ShowMessage(string text, MessageType messageType, bool uiThread = false)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override IAnimatableTimer GetAnimatableTimer()
+	{
+		return new AnimatableTimerMock();
+	}
+
+	public override void ExecuteOnUIThread(Action action)
+	{
+		action();
+	}
+
+        public override void ShowDialogWindow(object dataContext, Action onClose)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CloseDialogWindow()
+        {
+            throw new NotImplementedException();
+        }
+    }
