f2e94b35e5cbf3d0fbbffe42b144df6504459f7c
VladimirKhil <vladimir.khil@gmail.com>
2024-04-27 10:30:12 +0200

    Support run auto game request

---
+++ src/Common/SI.GameServer.Contract/RunAutoGameRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace SI.GameServer.Contract;
+
+/// <summary>
+/// Defines an automatic game run request.
+/// </summary>
+/// <param name="Culture">Game culture.</param>
+public sealed record RunAutoGameRequest(string Culture);
