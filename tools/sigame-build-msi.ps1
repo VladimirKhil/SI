abd432b6cd664938a77ef011772e3a893ddb153f
VladimirKhil <vladimir.khil@gmail.com>
2024-08-15 08:59:25 +0200

    Upgrade SIGame.Setup to Wix 5.0.1

--- tools/sigame-build-msi.ps1
+++ tools/sigame-build-msi.ps1
@@ -3,4 +3,4 @@ param (
     [string]$platform = "x64"
 )
 
-& "$Env:PROGRAMFILES\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" ..\deploy\SIGame.Setup\SIGame.Setup.wixproj /p:Configuration=Release /p:Platform=$platform /p:OutputPath=../../bin/.Release/SIGame.Setup/$platform /p:BuildProjectReferences=false /p:MsiProductVersion=$version
+dotnet build ..\deploy\SIGame.Setup\SIGame.Setup.wixproj /p:Configuration=Release /p:Platform=$platform /p:OutputPath=../../bin/.Release/SIGame.Setup/$platform /p:BuildProjectReferences=false /p:MsiProductVersion=$version
